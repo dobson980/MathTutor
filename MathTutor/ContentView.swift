7d622fcb5f58222a0ee2594d26c0e0faf9932c43
Tom Dobson <dobson980@gmail.com>
2024-07-11 08:41:14 -0700

    Initial Commit

---
+++ MathTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathTutor
+//
+//  Created by Thomas Dobson on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Thomas Dobson on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
